b99341f9163eeaccc9d6a2975e09e8fe7a482030
asphaltexistenzler <60293666+asphaltexistenzler@users.noreply.github.com>
2020-03-22 18:28:39 +0100

    Add files via upload

---
+++ simple User Log-in.py
@@ -0,0 +1,15 @@
+name='Simon'
+password='SimonZmugg'
+print('please type your username')
+Username=input()
+if name=='Simon':
+    print('Hello Simon')
+else:
+    print('Oh we don\'ţ know you\n do you want to create a new account?')
+    
+print('type your password')
+Password=input()
+if Password=='SimonZmugg':
+    print('Access granted')
+else:
+    print('wrong password')
